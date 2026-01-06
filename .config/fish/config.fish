db551852ab967c529772d7b2052edfc92bb92d26
Hikari Hayashi <rev.hikari@gmail.com>
2026-01-07 05:25:00 +0900

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,12 @@
 fish_add_path --global ~/.proto/bin
 fish_add_path --global ~/.proto/shims
 
+set --global --export BAT_PAGER 'less --+no-init --RAW-CONTROL-CHARS'
+set --global --export DELTA_PAGER 'less --+no-init --RAW-CONTROL-CHARS'
+
 if command --query zed
     set --global --export EDITOR 'zed --wait'
     set --global --export VISUAL 'zed --wait'
-end
-
-set --local less_version (less --version | string match --regex --groups-only 'less (\d+)')
-if test "$less_version" -ge 608
-    set --global --export BAT_PAGER 'less --+no-init --RAW-CONTROL-CHARS --quit-if-one-screen --redraw-on-quit'
-    set --global --export DELTA_PAGER 'less --+no-init --RAW-CONTROL-CHARS --quit-if-one-screen --redraw-on-quit'
 end
 
 if status is-interactive
