4f13aa36e49acde866bd8534b1729fb2c8df8c56
Hikari Hayashi <rev.hikari@gmail.com>
2026-06-21 09:21:21 +0800

    Use neovim as editor in terminal; clean up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,22 +7,17 @@ set --global --export BAT_PAGER 'less --+no-init --RAW-CONTROL-CHARS --quit-if-o
 set --global --export DELTA_PAGER 'less --+no-init --RAW-CONTROL-CHARS --quit-if-one-screen'
 
 if command --query zed
-    set --global --export EDITOR 'zed --wait'
-    set --global --export VISUAL 'zed --wait'
+    set --global --export EDITOR nvim
+    set --global --export VISUAL nvim
 end
 
 if status is-interactive
     set --global fish_greeting
+
     if contains 'Catppuccin Latte' (fish_config theme list | string split '\n')
         fish_config theme choose 'Catppuccin Latte'
     end
 
-    function fish_hybrid_key_bindings
-        for mode in default insert visual
-            fish_default_key_bindings --mode $mode
-        end
-        fish_vi_key_bindings --no-erase
-    end
     set --global fish_key_bindings fish_hybrid_key_bindings
 
     if command --query starship
